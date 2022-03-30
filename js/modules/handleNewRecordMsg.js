b0039935fb88c7088734327d9c621ab45205b668
Abdel Bailoul <ab.bailoul@gmail.com>
2022-03-30 10:04:04 +0200

    issue-img-new-record / Changed path

--- js/modules/handleNewRecordMsg.js
+++ js/modules/handleNewRecordMsg.js
@@ -1,16 +1,16 @@
 export function createNewRecordMsg() {
-  const container = document.querySelector(".container");
-  const elDiv = document.createElement("div");
-  elDiv.id = "new-record";
-  const imgRecord = document.createElement("img");
-  imgRecord.classList.add("new-record__img");
-  imgRecord.src = "../assets/img/record.svg";
+  const container = document.querySelector('.container');
+  const elDiv = document.createElement('div');
+  elDiv.id = 'new-record';
+  const imgRecord = document.createElement('img');
+  imgRecord.classList.add('new-record__img');
+  imgRecord.src = './assets/img/record.svg';
 
   elDiv.appendChild(imgRecord);
   container.appendChild(elDiv);
 }
 
 export function displayNewRecordMsg() {
-  const newRecord = document.getElementById("new-record");
-  newRecord.style.display = "block";
+  const newRecord = document.getElementById('new-record');
+  newRecord.style.display = 'block';
 }
